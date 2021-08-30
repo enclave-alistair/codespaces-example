a80b964b3185449646e27f7aad73a1494b9cfef9
Alistair Evans <alistair.evans@enclave.io>
2021-08-30 11:17:39 +0000

    Add simple .net project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace codespaces_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
